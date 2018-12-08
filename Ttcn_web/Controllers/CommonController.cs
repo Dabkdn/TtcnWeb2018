88bc1b8cc8bd81684391925f129be61fe9e6a116
Hà Nguyễn <nkh1111202@gmail.com>
2018-12-08 10:54:02 +0700

    commit 8/12/2018

---
+++ Ttcn_web/Controllers/CommonController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Ttcn_web.Controllers
+{
+    public class CommonController : Controller
+    {
+        public ActionResult ThietKeNoiThat()
+        {
+            return View();
+        }
+    }
+}
