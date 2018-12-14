df77e8f6074ab27cb90804a6ec6a186fff8290b6
TrangBam <binhlyoko@gmail.com>
2018-12-14 12:17:25 +0700

    no message

--- Ttcn_web/Services/Implementations/CartService .cs
+++ Ttcn_web/Services/Implementations/CartService .cs
@@ -16,13 +16,11 @@ namespace Ttcn_web.Services.Implementations
         public Carts GetCartsOfUser(int userID)
         {
             var objcart = new Carts();
-            ARCustomerDeliveryAddresss objCustomerDeliveryAddress = null;
             var objsaleOrder = db.ARSaleOrders.Where(p => p.FK_ADUserID == userID && p.ARSaleOrderStatus == "New" && p.AAStatus == "Alive").FirstOrDefault();
             var objCustomer = db.ADUsers.Find(userID).ARCustomers.ToList().FirstOrDefault();
             if(objCustomer != null)
             {
                 objcart.CustomerName = objCustomer.ARCustomerName;
-                objCustomerDeliveryAddress = db.ARCustomerDeliveryAddressses.Where(p => p.FK_ARCustomerID == objCustomer.ARCustomerID).FirstOrDefault();
             }
 
             if(objsaleOrder != null)
@@ -33,17 +31,7 @@ namespace Ttcn_web.Services.Implementations
                 objcart.FeeShipping = 0;
                 objcart.TotalAmount = objsaleOrder.ARSaleOrderTotalAmount.GetValueOrDefault();
                 objcart.SaleOrderItemList = objsaleOrder.ARSaleOrderItems.Where(p => p.AAStatus == "Alive").ToList();
-            }
-
-            if (objCustomerDeliveryAddress != null)
-            {
-                objcart.DeliveryAddress = objCustomer.ARCustomerAddress + objCustomer.ARCustomerContactPhone;
-
-                //objcart.DeliveryAddress = objCustomerDeliveryAddress.GEStreet.GEStreetName.FirstOrDefault() + " , "
-                //                        + objCustomerDeliveryAddress.GEWard.GEWardName.FirstOrDefault() + " , "
-                //                        + objCustomerDeliveryAddress.GEDistrict.GEDistrictName.FirstOrDefault() + " , "
-                //                        + objCustomerDeliveryAddress.GEStateProvince.GEStateProvinceName.FirstOrDefault() + " , "
-                //                        + objCustomerDeliveryAddress.ARCustomerDeliveryAddressContactPhone;
+                objcart.DeliveryAddress = objCustomer.ARCustomerAddress + " - " + objCustomer.ARCustomerContactPhone;
             }
             
             return (Carts)objcart;
