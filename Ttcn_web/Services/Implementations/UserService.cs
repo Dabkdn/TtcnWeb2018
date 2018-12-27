774d3a6766610cb1277dce317f6a7f7a37c901b6
Hà Nguyễn <nkh1111202@gmail.com>
2018-12-27 18:54:30 +0700

    commit 27/12/18

--- Ttcn_web/Services/Implementations/UserService.cs
+++ Ttcn_web/Services/Implementations/UserService.cs
@@ -12,15 +12,73 @@ namespace Ttcn_web.Services.Implementations
     {
         private TtcnWebEntities db = new TtcnWebEntities();
 
+        public ADUser Get(int userId)
+        {
+            return db.ADUsers.Find(userId);
+        }
+
         public IEnumerable<ADUser> GetAll()
         {
-            throw new NotImplementedException();
+            return db.ADUsers.Where(x => x.AAStatus == "Alive").ToList();
         }
 
         public ADUser GetUserByID(int userUD)
         {
             ADUser objUser = db.ADUsers.Where(user => user.ADUserID == userUD).FirstOrDefault();
             return objUser;
+        }
+
+        public void Create(FormCollection formCollection)
+        {
+            var user = new ADUser();
+            var lastUser = db.ADUsers.ToList().LastOrDefault();
+
+            user.ADUserID = lastUser.ADUserID + 1;
+            user.AAStatus = "Alive";
+            user.AACreatedDate = DateTime.Now;
+            user.ADUserName = formCollection["username"];
+            user.ADPassword = Common.GetMD5(formCollection["password"]);
+            user.FK_ADUserGroupID = 1;
+
+            if (user == null)
+            {
+                return;
+            }
+
+            db.ADUsers.Add(user);
+            db.SaveChanges();
+        }
+
+        public void Edit(FormCollection formCollection, int userId)
+        {
+            var user = db.ADUsers.Find(userId);
+
+            if (user == null)
+            {
+                return;
+            }
+
+            user.AAUpdatedDate = DateTime.Now;
+            user.ADUserName = formCollection["username"];
+            user.ADPassword = Common.GetMD5(formCollection["password"]);
+
+            db.ADUsers.AddOrUpdate();
+            db.SaveChanges();
+        }
+
+        public void Delete(int userId)
+        {
+            var user = db.ADUsers.Find(userId);
+
+            if (user == null)
+            {
+                return;
+            }
+
+            user.AAStatus = "Delete";
+
+            db.ADUsers.AddOrUpdate();
+            db.SaveChanges();
         }
     }
 }
